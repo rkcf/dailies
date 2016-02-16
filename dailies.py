4ea501b425e738c93e532c3ffed782d74dbcff88
zoombafu <apstinky@yahoo.com>
2016-02-16 08:24:30 -0600

    cleanned up comments

--- dailies.py
+++ dailies.py
@@ -1,20 +1,22 @@
+# Dailies
+# A simple command line utilite for keeping track of daily tasks
+
 import sys 
 
 task_list = []
 
+# TODO Load Data
 # Check for data file in $HOME/.dailies.
-
 # Create one if it does not exits.
-
 # Load data to python data structures.
 # name, times_completed, current_streak, max_streak, date_last_completed
 
-# Cleanup Streaks
+# TODO Cleanup Streaks
 # Checks each tasks date_last_complete against current date, if it is two
 # days previous, the current_streak is reset to 0.
 
-# Parse Arguments.
 def parse_args():
+    """Parses arguments from command line and invokes proper function"""
     arg_cnt = len(sys.argv) - 1
     if arg_cnt < 1 and arg_cnt > 2: 
         print(arg_cnt)
@@ -38,28 +40,23 @@ def parse_args():
         print('ERROR: Invalid command!')
         print_help()
 
-# List Command
-# Print each daily task along with max completions and current streak to the
-# terminal. Orderd by current streak.
 def list_all():
+    """Prints a table with tasks and stats."""
     print ('{0:5} | {1:5}'.format('Task', 'Total'))
     for task in task_list:
         print ('{0:5} | {1:5d}'.format(task['name'], task['times_completed']))
 
-# Today Command
-# Print each task that needs to be done yet today.
+#TODO Today Command: Print each task that needs to be done yet today.
 
-# Add Command
-# Accepts user input for new task name and adds it to list.
 def add_task(name):
+    """Adds new task to list."""
     task = {'name': name,
             'times_completed': 0,
            }
     task_list.append(task)
 
-# Remove Command
-# Prompts user to confirm, then removes task from list.
 def remove_task(task):
+    """Removes task from list."""
     if task_list.count(task) == 1:
         while True:
             confirm = input('Are you sure you want to permenantly delete this task [y/n]?')
@@ -73,18 +70,17 @@ def remove_task(task):
     else:
         print('ERROR: Task not found!')
 
-# Complete Command
-# Updates date_last_completed value of task to todays date.  Increments 
-# times_completed.  Adds one to current_streak, and updates max_streak as
-# needed.
 
-# Help Command
-# Prints command list
+#TODO Complete Command: Updates date_last_completed value of task to todays
+# date.  Increments times_completed.  Adds one to current_streak, and updates
+# max_streak as needed.
+
 def print_help():
+    """Prints list of commands with descriptions."""
     print('help')
 
 
-# Save changes
+#TODO Save changes to file
 
 def main():
     parse_args()
