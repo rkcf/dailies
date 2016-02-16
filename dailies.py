859f96a40868b793d47fdf58c808c7e0cab9ccc2
zoombafu <apstinky@yahoo.com>
2016-02-16 09:33:51 -0600

    wrote function for saving task data

--- dailies.py
+++ dailies.py
@@ -6,11 +6,11 @@ import os
 import csv
 
 task_list = []
-# name, times_completed, current_streak, max_streak, date_last_completed
+data_path = os.environ['HOME'] + '/.dailies'
+# name, total, current_streak, max_streak, date_last_completed
 
 def load_data():
     """Loads csv task data into dicts, and places them in the master list"""
-    data_path = os.environ['HOME'] + '/.dailies'
     if os.path.isfile(data_path):
         with open(data_path, 'r') as f:
             parser = csv.DictReader(f)
@@ -53,14 +53,14 @@ def list_all():
     """Prints a table with tasks and stats."""
     print ('{0:5} | {1:5}'.format('Task', 'Total'))
     for task in task_list:
-        print ('{0:5} | {1:5d}'.format(task['name'], task['times_completed']))
+        print ('{0:5} | {1:5}'.format(task['name'], task['total']))
 
 #TODO Today Command: Print each task that needs to be done yet today.
 
 def add_task(name):
     """Adds new task to list."""
     task = {'name': name,
-            'times_completed': 0,
+            'total': 0,
            }
     task_list.append(task)
 
@@ -81,18 +81,26 @@ def remove_task(task):
 
 
 #TODO Complete Command: Updates date_last_completed value of task to todays
-# date.  Increments times_completed.  Adds one to current_streak, and updates
+# date.  Increments total.  Adds one to current_streak, and updates
 # max_streak as needed.
 
 def print_help():
     """Prints list of commands with descriptions."""
     print('help')
 
-
-#TODO Save changes to file
+def save_data():
+    """Writes task data to file"""
+    with open(data_path, 'w') as f:
+        # name, total, current_streak, max_streak, date_last_completed
+        fieldnames = ['name', 'total']       
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for dict in task_list:
+            writer.writerow(dict) 
 
 def main():
     load_data()
     parse_args()
+    save_data()
 
 main()
