7036c77b6c9970720b1aa52efb60babaafa29dbc
zoombafu <apstinky@yahoo.com>
2016-02-16 05:56:37 -0600

    wrote planning skeleton for main program file

---
+++ dailies.py
@@ -0,0 +1,35 @@
+# Check for data file in $HOME/.dailies.
+
+# Create one if it does not exits.
+
+# Load data to python data structures.
+# name, times_completed, current_streak, max_streak, date_last_completed
+
+# Cleanup Streaks
+# Checks each tasks date_last_complete against current date, if it is two
+# days previous, the current_streak is reset to 0.
+
+# Parse Arguments.
+
+# List Option
+# Print each daily task along with max completions and current streak to the
+# terminal. Orderd by current streak.
+
+# Today Option
+# Print each task that needs to be done yet today.
+
+# Add Option
+# Accepts user input for new task name and adds it to list.
+
+# Remove Option
+# Prompts user to confirm, then removes task from list.
+
+# Complete Option
+# Updates date_last_completed value of task to todays date.  Increments 
+# times_completed.  Adds one to current_streak, and updates max_streak as
+# needed.
+
+# Help Option
+# Prints command list
+
+# Save changes
