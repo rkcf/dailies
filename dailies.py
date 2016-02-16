762227a5674a9b5766faa78c2877a0489de3795e
zoombafu <apstinky@yahoo.com>
2016-02-16 07:55:27 -0600

    wrote basic argument parsing with basic list, add, and help commands

--- dailies.py
+++ dailies.py
@@ -1,3 +1,7 @@
+import sys 
+
+task_list = []
+
 # Check for data file in $HOME/.dailies.
 
 # Create one if it does not exits.
@@ -10,26 +14,62 @@
 # days previous, the current_streak is reset to 0.
 
 # Parse Arguments.
+def parse_args():
+    arg_cnt = len(sys.argv) - 1
+    if arg_cnt < 1 and arg_cnt > 2: 
+        print(arg_cnt)
+        print('ERROR: Incorrect number of arguments!')
+        print_help()
+    elif sys.argv[1] == 'add': 
+        if arg_cnt == 2:
+            add_task(sys.argv[2])
+        else:
+            print('ERROR: add requires an argument!')
+    elif sys.argv[1] == 'list': 
+        list_all()
+    elif sys.argv[1] == 'help': 
+        print_help()
+    else:
+        print('ERROR: Invalid command!')
+        print_help()
 
-# List Option
+# List Command
 # Print each daily task along with max completions and current streak to the
 # terminal. Orderd by current streak.
+def list_all():
+    
+    print ('{0:5} | {1:5}'.format('Task', 'Total'))
+    for task in task_list:
+        print ('{0:5} | {1:5d}'.format(task['name'], task['times_completed']))
 
-# Today Option
+# Today Command
 # Print each task that needs to be done yet today.
 
-# Add Option
+# Add Command
 # Accepts user input for new task name and adds it to list.
+def add_task(name):
+    task = {'name': name,
+            'times_completed': 0,
+           }
+    task_list.append(task)
 
-# Remove Option
+# Remove Command
 # Prompts user to confirm, then removes task from list.
 
-# Complete Option
+# Complete Command
 # Updates date_last_completed value of task to todays date.  Increments 
 # times_completed.  Adds one to current_streak, and updates max_streak as
 # needed.
 
-# Help Option
+# Help Command
 # Prints command list
+def print_help():
+    print('help')
+
 
 # Save changes
+
+def main():
+    parse_args()
+
+main()
