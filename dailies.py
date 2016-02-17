a59d7d92de7a2e744cbff0a289526ee42f359d36
zoombafu <apstinky@yahoo.com>
2016-02-17 04:52:31 -0600

    more code cleaning

--- dailies.py
+++ dailies.py
@@ -7,13 +7,15 @@ import os
 import csv
 import datetime
 
-data_path = os.environ['HOME'] + '/.dailies'
+DATA_PATH = os.environ['HOME'] + '/.dailies'
+TODAY_ORD = datetime.date.today().toordinal()
 
 def load_data():
     """Loads csv task data into dicts, and places them in the master list."""
-    if os.path.isfile(data_path):
+    if os.path.isfile(DATA_PATH):
         task_list = []
-        with open(data_path, 'r') as f:
+        with open(DATA_PATH, 'r') as f:
+
             parser = csv.DictReader(f)
             for row in parser:
                 row['total'] = int(row['total'])
@@ -21,31 +23,31 @@ def load_data():
                 row['max_streak'] = int(row['max_streak'])
                 row['date_completed'] = int(row['date_completed'])
                 task_list.append(row)
-        return(task_list)
+        return task_list
     else:
         print('Datafile not found, creating one at $HOME/.dailies')
         save_data([])
-        return([])
+        return []
+
 
 def clean_streaks(list):
     """Checks whether any streaks are stale, and resets counter to 0 if so."""
-    today_ord = datetime.date.today().toordinal()
     for d in list:
-        if today_ord - d['date_completed'] > 1:
+        if TODAY_ORD - d['date_completed'] > 1:
             d['streak'] = 0
 
 def parse_args(list):
     """Parses arguments from command line and invokes proper function."""
     arg_cnt = len(sys.argv) - 1
-    if arg_cnt < 1 or arg_cnt > 2: 
+    if arg_cnt < 1 or arg_cnt > 2:
         print('ERROR: Incorrect number of arguments!')
         print_help()
-    elif sys.argv[1] == 'add': 
+    elif sys.argv[1] == 'add':
         if arg_cnt == 2:
             add_task(sys.argv[2], list)
         else:
             print('ERROR: add requires an argument!')
-    elif sys.argv[1] == 'complete': 
+    elif sys.argv[1] == 'complete':
         if arg_cnt == 2:
             complete_task(sys.argv[2], list)
         else:
@@ -55,19 +57,20 @@ def parse_args(list):
             remove_task(sys.argv[2], list)
         else:
             print('ERROR: rm requires an argument!')
-    elif sys.argv[1] == 'list': 
+    elif sys.argv[1] == 'list':
         list_all(list)
-    elif sys.argv[1] == 'help': 
+    elif sys.argv[1] == 'help':
         print_help()
     else:
         print('ERROR: Invalid command!')
         print_help()
 
+
 def list_all(list):
     """Prints a table with tasks and stats."""
-    print ('{0:10} | {1:5} | {2:6} | {3:5}'.format('Task', 'Total', 'Streak', 'Max Streak'))
+    print('{0:10} | {1:5} | {2:6} | {3:5}'.format('Task', 'Total', 'Streak', 'Max Streak'))
     for d in list:
-        print ('{0:10} | {1:5} | {2:6} | {3:5}'.format(d['name'], d['total'], d['streak'], d['max_streak']))
+        print('{0:10} | {1:5} | {2:6} | {3:5}'.format(d['name'], d['total'], d['streak'], d['max_streak']))
 
 
 def add_task(name, list):
@@ -105,12 +108,11 @@ def complete_task(name, list):
     for d in list:
         if d['name'] == name:
             d['total'] += 1
-            today_ord = datetime.date.today().toordinal()
-            if d['date_completed'] != today_ord:
-                d['streak'] +=1
+            if d['date_completed'] != TODAY_ORD:
+                d['streak'] += 1
             if d['streak'] > d['max_streak']:
                 d['max_streak'] = d['streak']
-            d['date_completed'] = today_ord
+            d['date_completed'] = TODAY_ORD
 
 def print_help():
     """Prints list of commands with descriptions."""
@@ -123,18 +125,18 @@ Commands:
     add     Add a new daily.  Takes a task name argument.
     rm      Permenantly remove a daily.  Takes a task name argument.
     complete Mark a task complete.  Takes a task name argument.
-    list    Display all dailies and their stats""" 
+    list    Display all dailies and their stats"""
 
     print(help_text)
 
 def save_data(list):
     """Writes list of dicts to file."""
-    with open(data_path, 'w') as f:
-        fieldnames = ['name', 'total', 'streak', 'max_streak', 'date_completed']       
+    with open(DATA_PATH, 'w') as f:
+        fieldnames = ['name', 'total', 'streak', 'max_streak', 'date_completed']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
         for d in list:
-            writer.writerow(d) 
+            writer.writerow(d)
 
 def dict_in_list(name, list):
     """Check if the dict is in the list."""
@@ -144,9 +146,11 @@ def dict_in_list(name, list):
     return False
 
 def main():
+    """Main body of dailies cli"""
     task_list = load_data()
     clean_streaks(task_list)
     parse_args(task_list)
     save_data(task_list)
+
 
 main()
